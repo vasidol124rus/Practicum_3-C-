56709eb051cdd0414df1638e76ee3c2e57553c02
vasidol124rus <vasekku25@gmail.com>
2023-11-30 16:17:40 +0700

    Добавил задачу 1

---
+++ Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Clear();
+int[] FillArray(int size)
+{
+    Console.Write("Включаем рандомайзер и получаем... ");
+    Console.WriteLine();
+    int[] array = new int[size];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 101);
+    }
+    return array;
+}
+void PrintArray(int[] ElemArray)
+{
+    Console.Write("Наш массив: ");
+    for(int i = 0; i < ElemArray.Length; i++)
+    {
+        Console.Write(ElemArray[i] + " ");
+    }
+}
+void Filter(int[] elements) 
+{ 
+    int[] n = new int[elements.Length];
+    Console.WriteLine(); 
+    Console.Write("Элементы массива, значения которых лежат в отрезке [20, 90]: "); 
+
+    for (int i = 0; i < elements.Length; i++) 
+    { 
+        if (elements[i] >= 20 && elements[i] <= 90) 
+        { 
+            Console.Write(elements[i] + " ");
+            n[i] = elements[i];
+        } 
+    }
+    int countWithoutZeros = n.Count(x => x != 0);
+    Console.WriteLine();
+    Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [20,90]: {countWithoutZeros}");
+}
+
+int sizeMass = 10;
+int[] mass = FillArray(sizeMass);
+PrintArray(mass);
+Filter(mass);
